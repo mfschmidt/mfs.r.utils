ff355f9e091eb7c4624375fc11946a27f2c01532
Mike Schmidt <mikeschmidt@schmidtgracen.com>
2017-11-15 09:56:06 -0500

    added more robust independent axis checking, allowed for custom coloring of multiple plots

--- R/mfs_scatter_from_models.R
+++ R/mfs_scatter_from_models.R
@@ -5,12 +5,14 @@
 #'     often discouraged in strong terms to do this, so this library requires that
 #'     the x axes in both models are identical before proceeding. With identical x
 #'     axes, I don't see the problem in viewing two dependent variables together.
-#' @param model1 A linear model, returned from glm()
-#' @param model2 A linear model, returned from glm()
+#' @param model1 A generalized linear model, returned from glm()
+#' @param model2 A generalized linear model, returned from glm()
 #' @param xlab Optionally, provide a label for the x axis
-#' @param y1lab Optionally, provide a label for the y axis
-#' @param y2lab Optionally, provide a label for the y axis
+#' @param y1lab Optionally, provide a label for the first y axis
+#' @param y2lab Optionally, provide a label for the second y axis
 #' @param title Optionally, provide a title to override the default
+#' @param scheme1 override the default "bluish" colors for plotting model1
+#' @param scheme2 override the default "reddish" colors for plotting model2
 #' @keywords scatter loess plot glm
 #' @export
 #' @examples
@@ -19,11 +21,32 @@
 #' mfs_scatter_from_models(glm(y~x,data=d), glm(z~x,data=d), xlab="Independent variable")
 
 
-mfs_scatter_from_models <- function(model1, model2, xlab="", y1lab="", y2lab="", title="") {
+mfs_scatter_from_models <- function(model1, model2, xlab="", y1lab="", y2lab="", title="", scheme1="bluish", scheme2="reddish") {
     
     # Ensure we have identical x-axes in each model
-    if (!(all.equal(model1$model[,2], model2$model[,2]))) {
-        print("Independent axes differ between two models. Plotting is not possible.")
+    if (length(model1$model[,2]) != length(model2$model[,2])) {
+        s="Multiple y-axes are almost always a bad idea, but can be useful to plot two dependent variables against the same independent variable. "
+        print(paste0(
+            s,
+            "In this case, independent axes are not even the same length between model1 and model2."
+        ))
+        return(NA)
+    }
+    if (range(model1$model[,2][1]) != range(model2$model[,2][1]) || 
+        range(model1$model[,2][2]) != range(model2$model[,2][2])) {
+        s="Multiple y-axes are almost always a bad idea, but can be useful to plot two dependent variables against the same independent variable. "
+        print(paste0(
+            s,
+            "In this case, different ranges [(",
+            range(model1$model[,2])[1],
+            "-",
+            range(model1$model[,2])[2],
+            ") vs (",
+            range(model2$model[,2])[1],
+            "-",
+            range(model2$model[,2])[2],
+            ")] are covered by model1 and model2."
+        ))
         return(NA)
     }
     
@@ -58,27 +81,30 @@ mfs_scatter_from_models <- function(model1, model2, xlab="", y1lab="", y2lab="",
     # Build the plot
     p <- ggplot2::ggplot(data=model1$data, ggplot2::aes_string(x=main_ind, y=main_dep))
     p <- p + ggplot2::geom_point(data=model1$data, ggplot2::aes_string(x=main_ind, y=main_dep),
-                                 color=color_schemes[["bluish"]][3], shape=3)
+                                 color=color_schemes[[scheme1]][3], shape=3)
     p <- p + ggplot2::geom_point(data=model2$data, ggplot2::aes_string(x=main_ind, y=secd_dep),
-                                 color=color_schemes[["reddish"]][3], shape=4)
+                                 color=color_schemes[[scheme2]][3], shape=4)
     p <- p + ggplot2::geom_smooth(data=model1$data, ggplot2::aes_string(x=main_ind, y=main_dep),
-                                  method="loess", linetype="dotted", color=color_schemes[["bluish"]][2])
+                                  method="loess", linetype="dotted", color=color_schemes[[scheme1]][2])
     p <- p + ggplot2::geom_smooth(data=model2$data, ggplot2::aes_string(x=main_ind, y=secd_dep),
-                                  method="loess", linetype="dotted", color=color_schemes[["reddish"]][2])
+                                  method="loess", linetype="dotted", color=color_schemes[[scheme2]][2])
     p <- p + ggplot2::geom_abline(intercept=coef(summary(model1))["(Intercept)","Estimate"],
                                   slope=coef(summary(model1))[main_ind,"Estimate"],
-                                  color=color_schemes[["bluish"]][1])
+                                  color=color_schemes[[scheme1]][1])
     p <- p + ggplot2::geom_abline(intercept=coef(summary(model2))["(Intercept)","Estimate"],
                                   slope=coef(summary(model2))[main_ind,"Estimate"],
-                                  color=color_schemes[["reddish"]][1])
+                                  color=color_schemes[[scheme2]][1])
     p <- p + ggplot2::theme_bw()
     p <- p + ggplot2::ggtitle(usable_title)
-    p <- p + mfs_annotate_empty(s=mfs_glm_string(model1), xs=model1$model[,2], ys=model1$model[,1],
-                                opp=FALSE, text_color=color_schemes[["bluish"]][1])
-    p <- p + mfs_annotate_empty(s=mfs_glm_string(model2), xs=model2$model[,2], ys=model2$model[,1],
-                                opp=TRUE, text_color=color_schemes[["reddish"]][1])
-    
-    # p <- p + ggplot2::scale_y_continuous(sec.axis = sec_axis(trans = ~ . * (max_stones / max_revenue), name = 'number of stones'), labels = dollar)
+    p <- p + mfs.r.utils::mfs_annotate_empty(s=mfs_glm_string(model1), xs=model1$model[,2], ys=model1$model[,1],
+                                opp=FALSE, text_color=color_schemes[[scheme1]][1])
+    p <- p + mfs.r.utils::mfs_annotate_empty(s=mfs_glm_string(model2), xs=model2$model[,2], ys=model2$model[,1],
+                                opp=TRUE, text_color=color_schemes[[scheme2]][1])
+    p <- p + ggplot2::theme(
+        axis.text.y = ggplot2::element_text(color=color_schemes[[scheme1]][1]),
+        axis.text.y.right = ggplot2::element_text(color=color_schemes[[scheme2]][1])
+    )
+    p <- p + scale_y_continuous(sec.axis = sec_axis(~.))
     
     return(p)
 }
